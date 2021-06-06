96413dd69a1c5376f3333b69cc8e92350ce480ce
khalidhamedi <>
2021-06-06 19:41:37 +0000

    apply forloop&while

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
-var username=prompt("Enter your name")
-var date_of_birth=prompt("Enter your year of birth")
-var the_date=2021-date_of_birth
-var fullname="Your name is " +username + " and your Age is "+the_date
+//#region
+// var username=prompt("Enter your name")
+// var date_of_birth=prompt("Enter your year of birth")
+// var the_date=2021-date_of_birth
+// var fullname="Your name is " +username + " and your Age is "+the_date
 
-var conf= confirm("Is this correct information? " +"\n Your Info is : "+fullname)
+// var conf= confirm("Is this correct information? " +"\n Your Info is : "+fullname)
 
-if(conf==true)
+// if(conf==true)
+// {
+//   alert("Great")
+// }
+// else
+// {
+//   alert("The page will be refreshed \n to try again ")
+//   document.location.reload()
+// }
+// var para=document.createElement("h5")
+// var node=document.createTextNode(fullname)
+// para.appendChild(node)
+// var element =document.getElementById("photo")
+// element.appendChild(para)
+//endregion
+
+alert("Hello my friend, can I take you for a tour of my website")
+alert("Before that, let me know about the top Arab capitals that you like best")
+
+var number_cities=prompt("You can tell me from 3 to 5 cities only.. How many cities would you like to tell me?")
+
+while(number_cities < 3 || number_cities > 5)
 {
-  alert("Great")
+alert("oops Whatch out ! The number must be between 1 and 5")
+var number_cities=prompt("You can tell me from 3 to 5 cities only.. How many cities would you like to tell me?")
 }
-else
+
+
+for (let index = 0; index < number_cities; index++) 
 {
-  alert("The page will be refreshed \n to try again ")
-  document.location.reload()
+var cityname= prompt("plz Enter the capital number "+(index+1))
+var newelement=document.createElement("li")
+var linode=document.createTextNode(cityname)
+newelement.appendChild(linode)
+document.getElementById("ul-bigphoto").appendChild(newelement).classList.add("ul-bigphoto")
 }
-var para=document.createElement("h5")
-var node=document.createTextNode(fullname)
-para.appendChild(node)
-var element =document.getElementById("photo")
-element.appendChild(para)
